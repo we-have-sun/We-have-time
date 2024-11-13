ae87287d1294925829482cc3d478da0c5aa6e437
Natalia Terlecka <natalia@wehavesun.com>
2024-11-13 10:15:52 +0000

    Initial Xcode setup

---
+++ We have time/We have time/We_have_time.h
@@ -0,0 +1,18 @@
+//
+//  We_Have_Time.h
+//  We Have Time
+//
+//  Created by Natalia Terlecka on 13/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for We_Have_Time.
+FOUNDATION_EXPORT double We_Have_TimeVersionNumber;
+
+//! Project version string for We_Have_Time.
+FOUNDATION_EXPORT const unsigned char We_Have_TimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <We_Have_Time/PublicHeader.h>
+
+
